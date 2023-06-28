bafc788441726afd7e31d3473e89a33493b7273b
Victor Faltings <victor.faltings@gmail.com>
2023-06-28 15:15:04 +0200

    added nixpkgs-stable input to flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
       "https://mirrors.bfsu.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
     ];
-
     extra-substituters = [
       # nix community's cache server
       "https://nix-community.cachix.org"
@@ -19,11 +18,11 @@
   };
 
   inputs = {
-    # There are many ways to reference flake inputs. The most widely used is github:owner/name/reference,
-    # which represents the GitHub repository URL + branch/commit-id/tag.
-
-    # Official NixOS package source, using nixos-unstable branch here
+    # Official NixOS package source, unstable branch used by default
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Official NixOS package source, stable branch for potential backups
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
 
     # home-manager, used for managing user configuration
     home-manager = {
@@ -32,22 +31,19 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, 
+    nixpkgs, 
+    nixpkgs-stable, 
+    home-manager, 
+  ... }@inputs: {
     nixosConfigurations = {
       # By default, NixOS will try to refer the nixosConfiguration with its hostname.
-      # so the system named `nixos-test` will use this configuration.
       # However, the configuration name can also be specified using `sudo nixos-rebuild switch --flake /path/to/flakes/directory#<name>`.
       # The `nixpkgs.lib.nixosSystem` function is used to build this configuration, the following attribute set is its parameter.
       # Run `sudo nixos-rebuild switch --flake .#nixos-test` in the flake's directory to deploy this configuration on any NixOS system
-      "nixos" = nixpkgs.lib.nixosSystem {
+      "nixos" = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
 
-        # The Nix module system can modularize configuration, improving the maintainability of configuration.
-        #
-        # Each parameter in the `modules` is a Nix Module, and there is a partial introduction to it in the nixpkgs manual:
-        #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
-        # It is said to be partial because the documentation is not complete, only some simple introductions
-        #    (such is the current state of Nix documentation...)
         # A Nix Module can be an attribute set, or a function that returns an attribute set.
         # If a Module is a function, this function can only have the following parameters:
         #
@@ -63,13 +59,19 @@
         #               this parameter is rarely used, you can ignore it for now.
         #
         # Only these parameters can be passed by default.
-        # If you need to pass other parameters, you must use `specialArgs` by uncomment the following line
-        # specialArgs = {...}  # pass custom arguments into sub module.
+
+        specialArgs = { # pass custom arguments into sub module.
+          pkgs-stable = import nixpkgs-stable {
+            system = system;
+            config.allowUnfree = true;
+          };
+        };
+
         modules = [
-          # Import the configuration.nix we used before, so that the old configuration file can still take effect.
-          # Note: /etc/nixos/configuration.nix itself is also a Nix Module, so you can import it directly here
+          # NixOS system configuration
           ./configuration.nix
 
+          # Home-manager module
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
