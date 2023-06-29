f12cf93c403409fc1b3a5dbc1b370a21b316e7e0
Victor Faltings <victor.faltings@gmail.com>
2023-06-29 14:15:32 +0200

    starship setup

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -7,14 +7,101 @@
 #       └─ ./starship.nix *
 #
 
-{ pkgs, pkgs-unstable, ... }:
+{ lib, pkgs, pkgs-unstable, ... }:
 
 {
   programs.starship = {
     enable = true;
-    options = {
+    enableTransience = true;
+    settings = {
+      add_newline = false;
       format = ''$character'';
-      right_format = ''$all'';
+      right_format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$localip"
+        "$shlvl"
+        "$singularity"
+        "$kubernetes"
+        "$directory"
+        "$vcsh"
+        "$fossil_branch"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_metrics"
+        "$git_status"
+        "$hg_branch"
+        "$pijul_channel"
+        "$docker_context"
+        "$package"
+        "$c"
+        "$cmake"
+        "$cobol"
+        "$daml"
+        "$dart"
+        "$deno"
+        "$dotnet"
+        "$elixir"
+        "$elm"
+        "$erlang"
+        "$fennel"
+        "$golang"
+        "$guix_shell"
+        "$haskell"
+        "$haxe"
+        "$helm"
+        "$java"
+        "$julia"
+        "$kotlin"
+        "$gradle"
+        "$lua"
+        "$nim"
+        "$nodejs"
+        "$ocaml"
+        "$opa"
+        "$perl"
+        "$php"
+        "$pulumi"
+        "$purescript"
+        "$python"
+        "$raku"
+        "$rlang"
+        "$red"
+        "$ruby"
+        "$rust"
+        "$scala"
+        "$solidity"
+        "$swift"
+        "$terraform"
+        "$vlang"
+        "$vagrant"
+        "$zig"
+        "$buf"
+        "$nix_shell"
+        "$conda"
+        "$meson"
+        "$spack"
+        "$memory_usage"
+        "$aws"
+        "$gcloud"
+        "$openstack"
+        "$azure"
+        "$env_var"
+        "$crystal"
+        "$custom"
+        "$sudo"
+        "$cmd_duration"
+        "$line_break"
+        "$jobs"
+        "$battery"
+        "$time"
+        "$status"
+        "$os"
+        "$container"
+        "$shell"
+        # "$character"
+      ];
     };
   };
 }
