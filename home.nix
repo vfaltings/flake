c6671b1183bd49dcbc85f2acc0c3e11870f0658c
Victor Faltings <victor.faltings@gmail.com>
2023-07-14 22:59:36 +0200

    added swayidle

--- home.nix
+++ home.nix
@@ -1,5 +1,25 @@
 { lib, config, pkgs, pkgs-stable, nixvim, ... }:
 
+# Scripts
+let
+  myswaylock = pkgs.writeShellScriptBin "myswaylock" ''
+    swaylock  \
+           --screenshots \
+           --clock \
+           --indicator \
+           --indicator-radius 100 \
+           --indicator-thickness 7 \
+           --effect-blur 7x5 \
+           --effect-vignette 0.5:0.5 \
+           --ring-color 3b4252 \
+           --key-hl-color 880033 \
+           --line-color 00000000 \
+           --inside-color 00000088 \
+           --separator-color 00000000 \
+           --grace 2 \
+           --fade-in 0.3
+  '';
+in
 {
   imports = (import ./modules/programs) ++ (import ./modules/services);
 
@@ -105,6 +125,8 @@
     eww-wayland
     swaybg
     swaylock-effects
+    myswaylock
+    swayidle
 
     python3
     jq
