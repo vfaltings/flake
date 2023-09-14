2456ac8d092718dde672aaed5ba4c2aa96a3757a
Victor Faltings <victor.faltings@gmail.com>
2023-09-14 14:13:31 +0200

    update flake

--- configuration.nix
+++ configuration.nix
@@ -148,6 +148,12 @@
   services.tlp = {
     enable = true;
     settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
       START_CHARGE_THRESH_BAT0 = 7;
       STOP_CHARGE_THRESH_BAT0 = 90;
       START_CHARGE_THRESH_BAT1 = 7;
